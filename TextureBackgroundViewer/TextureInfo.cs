503de4a8f4656eeb7bc00b092fedd8a8a1b44408
Painven <painven@gmail.com>
2025-03-15 11:32:54 +0300

    Add project files.

---
+++ TextureBackgroundViewer/TextureInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+namespace TextureBackgroundViewer;
+
+public class TextureInfo
+{
+    private readonly FileInfo fi;
+    private readonly int number;
+
+    public TextureInfo(FileInfo fi)
+    {
+        this.fi = fi;
+        this.number = number;
+    }
+
+    public string FullName => fi.FullName;
+    public string ShortName => fi.Name;
+}
